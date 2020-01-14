d4c4804c76a9e69367f0571c23f8afa40c249f85
Ivor <ivor.benderavage@gmail.com>
2020-01-14 14:18:34 -0500

    Added HTML skeleton

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 # All of our pages
 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/about')
 
 def index():
-    return "Hello, world!"
+    user = {'username' : 'Ivor'}
+    return render_template('title.html', title='Home', user=user)
