a667432c7721581c7176a1bf994b6a9087a0dad8
Ivor <ivor.benderavage@gmail.com>
2020-01-16 12:11:10 -0500

    Added form validation

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 # All of our pages
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -23,8 +23,11 @@ def about():
     ]
     return render_template('title.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('about'))
     return render_template('login.html', title='Sign In', form=form)
